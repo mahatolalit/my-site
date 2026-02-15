72d4d564d20172d53d2740e3161ae307602b4009
mahatolalit <lalitmahato20051024@gmail.com>
2026-02-15 10:45:20 +0530

    feat: integrate WindowControls component into Terminal for enhanced window management

--- src/windows/Terminal.jsx
+++ src/windows/Terminal.jsx
@@ -1,12 +1,13 @@
 import WindowWrapper from "#hoc/WindowWrapper.jsx";
 import { techStack } from "#constants";
 import { Check, Flag } from "lucide-react";
+import WindowControls from "#components/WindowControls";
 
 const Terminal = () => {
     return (
         <div>
             <div id="window-header">
-                <p>Window Controls</p>
+                <WindowControls target="terminal" />
                 <h2>Tech Stack</h2>
             </div>
 
